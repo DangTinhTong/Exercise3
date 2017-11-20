1197b70dbe0797bb335883a77fba03ffb5955264
admin <dangtinhtong@gmail.com>
2017-11-20 13:03:42 +0700

    first commit

--- Exercise3/main.m
+++ Exercise3/main.m
@@ -2,16 +2,23 @@
 //  main.m
 //  Exercise3
 //
-//  Created by admin on 11/20/17.
-//  Copyright © 2017 admin. All rights reserved.
+//  Created by Tống Đăng Tình on 11/20/17.
+//  Copyright © 2017 Tống Đăng Tình. All rights reserved.
 //
-
+// What output would you expect from the following program
 #import <Foundation/Foundation.h>
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Hello, World!");
+      //  NSLog(@"Hello, World!");
+        
+        char c ,d;
+       // NSLog(@"%c %c",c,d);
+        c='d';
+        d=c;
+        NSLog(@"d=%c",d); //d=d
+        
     }
     return 0;
 }
